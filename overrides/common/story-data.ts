2032f7cd195155c4298a41656c3c741cea3d5ab5
Alex Gurvich <alex.b.gurvich@gmail.com>
2023-11-13 21:42:23 -0500

    add "related stories" to each theme page

--- overrides/common/story-data.ts
+++ overrides/common/story-data.ts
@@ -26,3 +26,42 @@ export const visitLocationFeaturesStoryIds = [
   "earth_pulse",
   "immersive_earth"
 ];
+
+export const airQualityStoryIds = [
+  'introduction_air_quality',
+  'air_quality_and_covid19',
+  'extra_air_pollution_burden',
+  'exploring_nasa_aq_gis' 
+];
+
+export const agricultureStoryIds = [
+  'introduction_agriculture' 
+];
+
+export const biodiversityStoryIds = [
+  'introduction_biodiversity'
+];
+
+export const disastersStoryIds = [
+  "introduction_disasters"
+]
+
+export const energyStoryIds = [
+  "introduction_energy_generation"
+];
+
+export const greenhouseGasesStoryIds = [
+  "introduction_greenhouse_gases"
+];
+
+export const seaLevelRiseStoryIds = [
+  "introduction_sea_level_rise"
+];
+
+export const wildfiresStoryIds = [
+  "introduction_wildfires"
+];
+
+export const waterResourcesStoryIds = [
+  "introduction_water_resources"
+];
