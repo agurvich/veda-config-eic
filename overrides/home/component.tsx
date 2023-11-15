f9c130277e67ec4811114c0768cb8f029f807559
Hanbyul Jo <hanbyul@nyu.edu>
2023-11-15 11:57:39 -0500

    Make earth image as a background image

--- overrides/home/component.tsx
+++ overrides/home/component.tsx
@@ -7,7 +7,7 @@ import Hug from "$veda-ui-scripts/styles/hug";
 import { VarHeading } from "$veda-ui-scripts/styles/variable-components";
 import { variableGlsp } from "$veda-ui-scripts/styles/variable-utils";
 import Image from "$veda-ui-scripts/components/common/blocks/images";
-import ThemeCards from '../components/theme-cards';
+import ThemeCards from "../components/theme-cards";
 import { themeLandingPageIds } from "../common/story-data";
 import { ExpandLink } from "./expand-link";
 
@@ -47,8 +47,8 @@ const HomeDescription = styled(Hug)`
 const ContentContainer = styled.div`
   display: flex;
   flex-flow: column;
-  background-image: linear-gradient(#FFFFFF, #EDF3FC);
-`
+  background-image: linear-gradient(#ffffff, #edf3fc);
+`;
 
 const ThemeContent = styled.div`
   display: flex;
@@ -86,10 +86,13 @@ const BottomContent = styled(Hug)`
 const InfoImageContent = styled.div`
   display: flex;
   flex-flow: row;
-  background-color: #02225B; // @TODO: But where can I get this color?
-  color: #FFFFFF;
+  background-color: #02225b; // @TODO: But where can I get this color?
+  color: #ffffff;
   width: 100%;
-
+  height: 350px;
+  background-image: url(${RedEarthImg});
+  background-position: right;
+  background-repeat: no-repeat;
   div {
     display: flex;
     flex-flow: column;
@@ -101,7 +104,7 @@ const InfoImageContent = styled.div`
   a {
     width: 18.5rem;
   }
-`
+`;
 
 const CollaboratorsContent = styled.div`
   display: flex;
@@ -121,21 +124,22 @@ const CollaboratorsContent = styled.div`
   p {
     text-align: center;
   }
-`
+`;
 
 export default function HomeComponent() {
-  const description = "The Earth Information Center consolidates data and insights on how Earth is changing from across the U.S. federal government. Discover how this data is being used to prepare for climate change, and mitigate, adapt, and respond to environmental challenges accross the country."
+  const description =
+    "The Earth Information Center consolidates data and insights on how Earth is changing from across the U.S. federal government. Discover how this data is being used to prepare for climate change, and mitigate, adapt, and respond to environmental challenges accross the country.";
   return (
     <>
       <HomeDescription>
         <IntroHeadline>
           <div>
-            <VarHeading size="xlarge">Multiple agencies,<br/> many interconnected systems, <br/>
+            <VarHeading size="xlarge">
+              Multiple agencies,
+              <br /> many interconnected systems, <br />
               <span>one planet.</span>
             </VarHeading>
-            <p>
-              {description}
-            </p>
+            <p>{description}</p>
           </div>
           <Partners size="small" />
         </IntroHeadline>
@@ -143,59 +147,66 @@ export default function HomeComponent() {
       <ContentContainer>
         <ThemeContent>
           <div>
-            <VarHeading size="large">
-              Nine themes, one Earth
-            </VarHeading>
+            <VarHeading size="large">Nine themes, one Earth</VarHeading>
             <Button
-                forwardedAs={NavLink}
-                to="/stories" //@TODO: THIS NEEDS TO BE REPLACED
-                size="medium"
-                radius="square"
-                variation="primary-fill"
-              >
+              forwardedAs={NavLink}
+              to="/stories" //@TODO: THIS NEEDS TO BE REPLACED
+              size="medium"
+              radius="square"
+              variation="primary-fill"
+            >
               View all themes
             </Button>
           </div>
           <ThemeCards storyIds={themeLandingPageIds} />
         </ThemeContent>
         <BottomContent>
+          <p>
+            Earth.gov is also the gateway to other interagency cooperative
+            efforts for our planet, like the{" "}
+            <ExpandLink to={`https://ghg.center/`}>
+              {" "}
+              U.S. Greenhouse Gas Center
+            </ExpandLink>
+          </p>
+          <InfoImageContent>
+            <div>
+              <VarHeading size="small">U.S. Greenhouse Gas Center</VarHeading>
+              <span>
+                Uniting Data and Technology to Empower Tomorrow's Climate
+                Solutions
+              </span>
+              <Button
+                forwardedAs={NavLink}
+                to="https://hub.ghg.center/hub/" //@TODO: This isn't working with external link
+                size="medium"
+                radius="square"
+                variation="primary-fill"
+              >
+                Visit the U.S. GHG Center website
+              </Button>
+            </div>
+          </InfoImageContent>
+          <CollaboratorsContent>
+            <VarHeading size="small">
+              Joining forces <span>for a better tomorrow</span>
+            </VarHeading>
             <p>
-              Earth.gov is also the gateway to other interagency cooperative efforts for our planet, like the <ExpandLink to={`https://ghg.center/`}> U.S. Greenhouse Gas Center</ExpandLink>
+              The Earth Information Center is grateful for the support and
+              expertise of our valued, multi-agency collaborators: EPA, FEMA,
+              NASA, NOAA, USAID, USDA, and USGS.
             </p>
-            <InfoImageContent>
-              <div>
-                <VarHeading size="small">
-                  U.S. Greenhouse Gas Center
-                </VarHeading>
-                <span>Uniting Data and Technology to Empower Tomorrow's Climate Solutions</span>
-                <Button
-                  forwardedAs={NavLink}
-                  to="https://hub.ghg.center/hub/" //@TODO: This isn't working with external link
-                  size="medium"
-                  radius="square"
-                  variation="primary-fill"
-                >
-                  Visit the U.S. GHG Center website
-                </Button>
-              </div>
-              <Image src={RedEarthImg} alt="USGS logo" height={300} />
-            </InfoImageContent>
-            <CollaboratorsContent>
-              <VarHeading size="small">
-                Joining forces <span>for a better tomorrow</span>
-              </VarHeading>
-              <p>The Earth Information Center is grateful for the support and expertise of our valued, multi-agency collaborators: EPA, FEMA, NASA, NOAA, USAID, USDA, and USGS.</p>
-              <Partners size="small" />
-                <Button
-                    forwardedAs={NavLink}
-                    to="https://hub.ghg.center/hub/" //@TODO: THIS NEEDS TO BE REPLACED
-                    size="medium"
-                    radius="square"
-                    variation="primary-fill"
-                  >
-                  Learn more
-                </Button>
-            </CollaboratorsContent>
+            <Partners size="small" />
+            <Button
+              forwardedAs={NavLink}
+              to="https://hub.ghg.center/hub/" //@TODO: THIS NEEDS TO BE REPLACED
+              size="medium"
+              radius="square"
+              variation="primary-fill"
+            >
+              Learn more
+            </Button>
+          </CollaboratorsContent>
         </BottomContent>
       </ContentContainer>
     </>
